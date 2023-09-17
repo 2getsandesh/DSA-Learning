17b397c991ec951af0e794db8e28f6342fd45226
Sandesh R <2getsandesh@gmail.com>
2023-09-17 12:25:40 +0530

    Update max.py

--- Week 1/max.py
+++ Week 1/max.py
@@ -6,3 +6,5 @@ greatest= max(a,b,c)
 smallest = min(a,b,c)
 print(f"The greatest is among {a}, {b} and {c} is {greatest}")
 print(f"The smallest is among {a}, {b} and {c} is {smallest}")
+
+# a if (a>b and a>c) else (b if (b>a and b>c) else c) to find greatest
