819bd24d8d8e32e562d7d4ba3d8dc9897bff6992
Sandesh R <2getsandesh@gmail.com>
2023-09-17 12:18:04 +0530

    Update max.py

--- Week 1/max.py
+++ Week 1/max.py
@@ -1,5 +1,8 @@
 a=5
 b=25
+c=10
 
-greatest= max(a,b)
-print(f"The greatest is among {a} and {b} is {greatest}")
+greatest= max(a,b,c)
+smallest = min(a,b,c)
+print(f"The greatest is among {a}, {b} and {c} is {greatest}")
+print(f"The smallest is among {a}, {b} and {c} is {smallest}")
