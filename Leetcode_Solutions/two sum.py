56f2eb5ff9236013da7a092862502528041ba568
Sandesh R <2getsandesh@gmail.com>
2023-09-19 22:33:42 +0530

    Create two sum.py

---
+++ Leetcode_Solutions/two sum.py
@@ -0,0 +1,9 @@
+'''Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
