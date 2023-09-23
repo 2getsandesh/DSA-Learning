26d2aac25a092e623be189a785de8dad2d77e73e
Sandesh R <2getsandesh@gmail.com>
2023-09-23 20:02:58 +0530

    Update needle haystack.py

--- Leetcode_Solutions/needle haystack.py
+++ Leetcode_Solutions/needle haystack.py
@@ -6,3 +6,17 @@ class Solution:
             return haystack.find(needle,0)
         else:
             return -1
+        
+
+#--------------------------------------------------------------#
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        i,j=0,len(needle)
+        while j<=len(haystack):
+            curr = haystack[i:j]
+            if curr==needle: return i
+            else:
+                i+=1
+                j+=1
+        return -1
