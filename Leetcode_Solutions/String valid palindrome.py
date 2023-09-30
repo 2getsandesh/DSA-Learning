21b0511964a54dfb83ec6f12cacd3c3df54cbd9a
Sandesh R <2getsandesh@gmail.com>
2023-09-30 19:57:59 +0530

    bla

--- Leetcode_Solutions/String valid palindrome.py
+++ Leetcode_Solutions/String valid palindrome.py
@@ -20,3 +20,10 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s=[i for i in s.lower() if i.isalnum()]
         return s==s[::-1]
+    
+#--------------------------------------------------#
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s="".join(i for i in s if i.isalnum())
+        s=s.lower()
+        if s==s[::-1]: return True
