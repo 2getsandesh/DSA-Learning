4f6a6c06853fe70b669c35445f6ef08d028ce632
Sandesh R <2getsandesh@gmail.com>
2023-10-02 11:57:59 +0530

    Update majority element.py

--- Leetcode_Solutions/majority element.py
+++ Leetcode_Solutions/majority element.py
@@ -7,3 +7,18 @@ class Solution:
         nums.sort()
         n = len(nums)
         return nums[n//2]
+    
+
+#----------------------------------------------------------#
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dic = {}
+        n = len(nums)//2
+        for key in nums:
+            if key in dic:
+                dic[key]+=1
+            else:
+                dic[key]=1
+
+        return max(dic.items(),key= lambda x: x[1])[0]
