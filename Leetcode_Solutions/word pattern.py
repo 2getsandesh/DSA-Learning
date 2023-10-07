21f830ae6eeb798e2a5f53bb77d50c0e5eb4cb00
Sandesh R <2getsandesh@gmail.com>
2023-10-07 23:42:54 +0530

    Update word pattern.py

--- Leetcode_Solutions/word pattern.py
+++ Leetcode_Solutions/word pattern.py
@@ -16,3 +16,25 @@ class Solution:
                 if j in dic.values(): return False
                 else: dic[i] = j
         return True
+    
+
+
+    #----------------------------------------------------------#
+
+
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        
+        a = list(pattern)
+        b = list(s.split(' '))
+        if len(a)!= len(b): return False
+        if len(set(a))!= len(set(b)): return False
+        dic = {}
+
+        for i,j in zip(a,b):
+            if i in dic:
+                if dic[i] != j: return False
+            else:
+                dic[i] = j
+        return True 
