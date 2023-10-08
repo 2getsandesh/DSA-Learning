fbe5e362f206dad40e88ea6f634c6d961bebfec0
Sandesh R <2getsandesh@gmail.com>
2023-10-08 16:57:46 +0530

    Update Excel sheet column number.py

--- Leetcode_Solutions/Excel sheet column number.py
+++ Leetcode_Solutions/Excel sheet column number.py
@@ -4,3 +4,12 @@ class Solution:
         for i in columnTitle:
             col = col*26 + ord(i) - 64
         return col 
+    
+#-----------------------------------------------------#
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        col = 0
+        for i in columnTitle:
+            col = col*26 + ord(i) - ord('A')+1
+        return col 
