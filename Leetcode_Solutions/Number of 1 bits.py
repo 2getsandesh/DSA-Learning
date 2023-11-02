f651163b699d35746df71bc19eefe33d9a651ed7
Sandesh R <2getsandesh@gmail.com>
2023-11-02 21:07:35 +0530

    Create Number of 1 bits.py

---
+++ Leetcode_Solutions/Number of 1 bits.py
@@ -0,0 +1,12 @@
+'''Write a function that takes the binary representation of an unsigned integer and 
+returns the number of '1' bits it has (also known as the Hamming weight).'''
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        s = str(bin(n))
+        count = 0
+        for i in s:
+            if i == '1':
+                count += 1
+        return count
+        
