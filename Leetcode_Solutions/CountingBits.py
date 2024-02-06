ec3bbdc84b29ed962fbcf606e73fbe3dbaf65f10
Sandesh R <2getsandesh@gmail.com>
2024-02-06 14:13:59 +0530

    Create CountingBits.py

---
+++ Leetcode_Solutions/CountingBits.py
@@ -0,0 +1,13 @@
+'''Given an integer n, return an array ans 
+of length n + 1 such that for each i (0 <= i <= n),
+ ans[i] is the number of 1's in the binary representation of i.'''
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans=[]
+        
+        for i in range(0,n+1):
+            c = bin(i).count("1")
+            ans.append(c)
+            
+        return ans
