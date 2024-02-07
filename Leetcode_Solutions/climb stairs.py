1251fa5076db67fb70c5895402893af82ca642d6
Sandesh R <2getsandesh@gmail.com>
2024-02-07 12:46:26 +0530

    Update climb stairs.py

--- Leetcode_Solutions/climb stairs.py
+++ Leetcode_Solutions/climb stairs.py
@@ -15,11 +15,11 @@ class Solution:
     
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n==0 or n==1:
-            return 1
-        dp = [0]*(n+1)
-        dp[0] = dp[1] = 1
+        if n==1:return 1
+        if n==2: return 2
+        dp = [0]*(n)
+        dp[0],dp[1] = 1,2
         
-        for i in range(2,n+1):
+        for i in range(2,n):
             dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
+        return dp[n-1]
