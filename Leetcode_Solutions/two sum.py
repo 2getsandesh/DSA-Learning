d36ae1d56dee9e3952b347fd5c494ff9db9f5d48
Sandesh R <2getsandesh@gmail.com>
2024-06-02 15:17:49 +0530

    Update two sum.py

--- Leetcode_Solutions/two sum.py
+++ Leetcode_Solutions/two sum.py
@@ -7,3 +7,19 @@ class Solution:
             for j in range(i+1,n):
                 if nums[i]+nums[j]==target:
                     return [i,j]
+                
+
+#-----------------Hash Table----------------------------
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            numMap[nums[i]]=i
+
+        for i in range(n):
+            comp = target - nums[i]
+            if comp in numMap and numMap[comp]!=i:
+                return [i,numMap[comp]]
