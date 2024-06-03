a6fd7b932fb5590b957e5c149fa9ee1214dc4056
Sandesh R <2getsandesh@gmail.com>
2024-06-03 14:35:55 +0530

    Update MaximumSubarray.py

--- Leetcode_Solutions/MaximumSubarray.py
+++ Leetcode_Solutions/MaximumSubarray.py
@@ -3,11 +3,9 @@ subarray with the largest sum, and return its sum.'''
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        currsum = nums[0]
-        maxsum = nums[0]
-
+        res = maxsum = nums[0]
         for i in nums[1:]:
-            currsum = max(i, currsum + i)
-            maxsum = max(maxsum, currsum)
-        return maxsum
+            maxsum = max(i,maxsum+i)
+            res = max(res,maxsum)
+        return res
         
