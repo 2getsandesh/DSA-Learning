8cc631e747c06a0ad8fe339aee702fc6d8e4af5c
Sandesh R <2getsandesh@gmail.com>
2024-06-05 13:51:13 +0530

    file changed

--- Leetcode_Solutions/Strings/Valid Anagram.py
+++ Leetcode_Solutions/Strings/Valid Anagram.py
@@ -13,3 +13,15 @@ class Solution:
         for i in set(s):
             if s.count(i)!=t.count(i): return False
         return True
+    
+#----------------------------------------------------------#
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq1 = [0]*26
+        freq2 = [0]*26
+        for i in s:
+            freq1[ord(i)-97]+=1
+        for i in t:
+            freq2[ord(i)-97]+=1
+        if freq1 == freq2: return True
+        else: return False
