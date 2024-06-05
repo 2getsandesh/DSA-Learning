92049c96e1d24dbfed5f58ddc20ff3cf79d51ba0
Sandesh R <2getsandesh@gmail.com>
2024-06-05 13:03:26 +0530

    Create longest repeating character replacement.py

---
+++ Leetcode_Solutions/Strings/longest repeating character replacement.py
@@ -0,0 +1,17 @@
+'''You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character.
+ You can perform this operation at most k times.
+Return the length of the longest substring containing the same letter you can get after performing the above operations.'''
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l=r=longest = 0
+        freq = [0]*26
+
+        for r in range(len(s)):
+            
+            freq[ord(s[r])-65] += 1
+            while (r-l+1) - max(freq)>k: 
+                freq[ord(s[l])-65] -= 1
+                l+=1
+            longest = max(r-l+1, longest)
+        return longest
