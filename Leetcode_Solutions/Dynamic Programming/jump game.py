8a2313c32c9f9539e6f665183ff322b764291f45
Sandesh R <2getsandesh@gmail.com>
2024-06-11 12:44:02 +0530

    Create jump game.py

---
+++ Leetcode_Solutions/Dynamic Programming/jump game.py
@@ -0,0 +1,15 @@
+'''You are given an integer array nums. You are initially positioned at the array's first index, 
+and each element in the array represents your maximum jump length at that position.
+Return true if you can reach the last index, or false otherwise.'''
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        goal = n-1
+        for i in range(n-2,-1,-1):
+            if i+nums[i] >= goal: goal = i
+
+        return True if goal==0 else False
+
+
+        
