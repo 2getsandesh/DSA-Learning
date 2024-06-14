baaf9c9b22e74a13825d866e8dec9d309f52c4a7
Sandesh R <2getsandesh@gmail.com>
2024-06-14 14:48:55 +0530

    Update maximum depth of binary tree.py

--- Leetcode_Solutions/Tree/maximum depth of binary tree.py
+++ Leetcode_Solutions/Tree/maximum depth of binary tree.py
@@ -11,3 +11,21 @@ class Solution:
         if not root: return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+#------------------------Iterative BFS---------------------------------------------#
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        q = deque([root])
+        depth = 0
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+            depth += 1
+        return depth
+    
+#--------------------------Iterative DFS-------------------------------------#
