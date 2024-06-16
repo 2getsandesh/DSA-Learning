4510ae7a86fc8109049a42acbc249b11d353abdf
Sandesh R <2getsandesh@gmail.com>
2024-06-16 15:04:37 +0530

    Create lowest common ancestor of Binary Tree.py

---
+++ Leetcode_Solutions/Tree/lowest common ancestor of Binary Tree.py
@@ -0,0 +1,13 @@
+'''LCA for just Binary Tree'''
+# Note: This is different from LCA for Binary SEARCH Tree
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root: return None
+        
+        if root is p or root is q: return root
+        
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+
+        if left and right: return root
+        return left if left else right
