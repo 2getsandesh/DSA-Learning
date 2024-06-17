29462f05d61987f2f87567efe0225c7588d4ec3e
Sandesh R <2getsandesh@gmail.com>
2024-06-17 09:55:49 +0530

    bla

--- Leetcode_Solutions/Binary/counting bits.py
+++ Leetcode_Solutions/Binary/counting bits.py
@@ -12,8 +12,10 @@ class Solution:
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        if not n: return [0]
-        dp = [0]
-        for i in range(1,n+1):
-                dp.append(dp[i//2]+i%2)
-        return dp
+        ans=[]
+        
+        for i in range(0,n+1):
+            c = bin(i).count("1")
+            ans.append(c)
+            
+        return ans
