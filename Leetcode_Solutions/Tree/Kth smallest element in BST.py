70fea9485931f9e433aaf3b48a2c241531d76aa9
Sandesh R <2getsandesh@gmail.com>
2024-06-18 11:14:36 +0530

    Update Kth smallest element in BST.py

--- Leetcode_Solutions/Tree/Kth smallest element in BST.py
+++ Leetcode_Solutions/Tree/Kth smallest element in BST.py
@@ -11,3 +11,18 @@ class Solution:
         
         inorder(root)
         return li[-1]
+    
+#================================Iterative===============================================#
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = [root]
+        cur = root
+
+        while stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            k -= 1
+            if k==0: return cur.val
+            cur = cur.right
