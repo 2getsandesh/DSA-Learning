b58bf69c1662d574a15cd493bb066b052105de62
Sandesh R <2getsandesh@gmail.com>
2024-06-24 11:36:21 +0530

    Update Valid sudoku.py

--- Leetcode_Solutions/Arrays/Valid sudoku.py
+++ Leetcode_Solutions/Arrays/Valid sudoku.py
@@ -18,3 +18,22 @@ class Solution:
                 cols[c].add(board[r][c])
                 square[(r//3,c//3)].add(board[r][c])
         return True
+    
+
+#==========================================================================================================================#
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = {i: set() for i in range(9)}
+        cols = {i: set() for i in range(9)}
+        square = {(i, j): set() for i in range(3) for j in range(3)}
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".": continue
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in square[(r//3,c//3)]:
+                    return False
+                rows[r].add(board[r][c]) 
+                cols[c].add(board[r][c])
+                square[(r//3,c//3)].add(board[r][c])
+        return True
